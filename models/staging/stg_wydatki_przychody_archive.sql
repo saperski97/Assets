877be004e4339248ff4b6d882063dac21629fa68
przemeksapkowski@gmail.com <przemeksapkowski@gmail.com>
2023-04-04 20:14:57 +0000

    Changed logic in stg_ and fct_ queries

---
+++ models/staging/stg_wydatki_przychody_archive.sql
@@ -0,0 +1,26 @@
+SELECT
+    string_field_0 AS Data,
+    string_field_1 AS Osoba,
+    string_field_2 AS Jedzenie,
+    string_field_3 AS Transport,
+    string_field_4 AS Czynsz,
+    string_field_5 AS Wyjscia,
+    string_field_6 AS ArtDomowe,
+    string_field_7 AS Alkohol,
+    string_field_8 AS Kosmetyki,
+    string_field_9 AS Nauka,
+    string_field_10 AS Wyjazdy,
+    string_field_11 AS Prezenty,
+    string_field_12 AS Ubrania,
+    string_field_13 AS Ksiazki,
+    string_field_14 AS Zdrowie,
+    string_field_15 AS Pozostale,
+    string_field_16 AS WystrojMieszkania,
+    string_field_17 AS Abonamenty,
+    string_field_18 AS Kredyt,
+    string_field_19 AS Rozliczenia,
+    string_field_20 AS Wydatki,
+    string_field_23 AS Przychody,
+    string_field_26 AS Bilans
+FROM `assets-382517.google_spreadsheet.wydatki_przychody_archive_T`
+WHERE string_field_0 LIKE"%'%"
